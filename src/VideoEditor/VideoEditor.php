56066cffac8e32684bba4d28fd0f34ef492258ad
michele <capimichi@gmail.com>
2019-03-12 23:02:27 +0100

    Fix filepath empty

--- src/VideoEditor/VideoEditor.php
+++ src/VideoEditor/VideoEditor.php
@@ -4,19 +4,20 @@ namespace VideoEditor;
 
 class VideoEditor
 {
-
+    
     /**
      * @var string
      */
     protected $ffmpegPath;
-
+    
     /**
      * @var boolean
      */
     protected $debug;
-
+    
     /**
      * VideoEditor constructor.
+     *
      * @param string $ffmpegPath
      */
     public function __construct($ffmpegPath)
@@ -24,55 +25,62 @@ class VideoEditor
         $this->ffmpegPath = $ffmpegPath;
         $this->debug = false;
     }
-
+    
     /**
      * @return Model\VideoInterface
      */
     public function createVideo($filePath = null)
     {
         $video = new \VideoEditor\Model\Video($this);
-
+        
         if ($filePath) {
             $video->setFilePath($filePath);
+            
+            if (!file_exists($filePath)) {
+                file_put_contents($filePath, "");
+            }
         }
-
+        
+        
         return $video;
     }
-
+    
     /**
      * @param $videos
+     *
      * @return Model\VideoInterface
      */
     public function concatenateVideos($videos)
     {
         $outputFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . md5(rand(0, 999999999)) . ".mp4";
-
+        
         $videosFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . md5(rand(0, 999999999)) . ".txt";
-
+        
         $f = fopen($videosFilePath, "w");
         foreach ($videos as $video) {
             fwrite($f, sprintf("file '%s'\n", $video->getFilePath()));
         }
         fclose($f);
-
+        
         $command = sprintf("%s %s -f concat -safe 0 -i %s -c copy \"%s\"",
             $this->ffmpegPath,
             $this->isDebug() ? "" : "-loglevel panic",
             $videosFilePath,
             $outputFile
         );
-
+        
         exec($command);
-
+        
         $outputVideo = $this->createVideo($outputFile);
-
+        
         return $outputVideo;
     }
-
+    
     /**
      * @param Model\VideoInterface $video
-     * @param $imagePath
-     * @param array $options
+     * @param                      $imagePath
+     * @param array                $options
+     *
      * @return Model\VideoInterface
      */
     public function addVideoFrame(\VideoEditor\Model\VideoInterface $video, $imagePath, $options = [])
@@ -80,9 +88,9 @@ class VideoEditor
         $options = array_merge([
             'duration' => 1,
         ], $options);
-
+        
         $outputFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . md5(rand(0, 999999999)) . ".mp4";
-
+        
         $command = sprintf("%s %s -loop 1 -i \"%s\" -c:v libx264 -t %s -pix_fmt yuv420p -vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\" \"%s\"",
             $this->ffmpegPath,
             $this->isDebug() ? "" : "-loglevel panic",
@@ -90,11 +98,11 @@ class VideoEditor
             $options['duration'],
             $outputFile
         );
-
+        
         exec($command);
-
+        
         $lastPieceVideo = $this->createVideo($outputFile);
-
+        
         if (filesize($video->getFilePath())) {
             $contatenatedVideo = $this->concatenateVideos([
                 $video,
@@ -106,24 +114,25 @@ class VideoEditor
             return $lastPieceVideo;
         }
     }
-
+    
     /**
      * @param Model\VideoInterface $video
-     * @param $text
-     * @param array $options
+     * @param                      $text
+     * @param array                $options
+     *
      * @return Model\VideoInterface
      */
     public function addVideoText(\VideoEditor\Model\VideoInterface $video, $text, $options = [])
     {
-
+        
         $options = array_merge([
             'start'     => "0",
             'duration'  => "1",
             'font_path' => "",
         ], $options);
-
+        
         $outputFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . md5(rand(0, 999999999)) . ".mp4";
-
+        
         $command = sprintf("%s %s -i %s -vf \"drawtext=enable='between(t,%s,%s)':fontfile=%s: text='%s'\" -acodec copy %s",
             $this->ffmpegPath,
             $this->isDebug() ? "" : "-loglevel panic",
@@ -134,14 +143,14 @@ class VideoEditor
             $text,
             $outputFile
         );
-
+        
         exec($command);
-
+        
         $video = $this->createVideo($outputFile);
-
+        
         return $video;
     }
-
+    
     /**
      * @return string
      */
@@ -149,7 +158,7 @@ class VideoEditor
     {
         return $this->ffmpegPath;
     }
-
+    
     /**
      * @return bool
      */
@@ -157,9 +166,10 @@ class VideoEditor
     {
         return $this->debug;
     }
-
+    
     /**
      * @param bool $debug
+     *
      * @return VideoEditor
      */
     public function setDebug($debug)
@@ -167,6 +177,6 @@ class VideoEditor
         $this->debug = $debug;
         return $this;
     }
-
-
+    
+    
 }
